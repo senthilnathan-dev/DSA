5322f2c6c9766aa7892e8b87d1f642771489cbec
senthilnathan <libertariansenthil@gmail.com>
2022-03-31 06:57:25 +0530

    initial C commit

---
+++ C/Array.c
@@ -0,0 +1,18 @@
+// Array data structure
+
+#include <stdio.h>
+
+void main()
+{
+    int a[5];
+    for (int i=0;i<5;i++)
+    {
+        a[i]= i;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
